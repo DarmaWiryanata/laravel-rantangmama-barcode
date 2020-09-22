310ad1b5b2c3c753a63eb1c0967a079669e2781d
Ida Bagus Kadek Darma Wiryatama <wirya_tama99@hotmail.com>
2020-09-22 23:17:37 +0800

    Revert "Keterangan Produk Retur/Rusak 2"
    This reverts commit df14d87605174191d186188ea2d467a7d6acf9ca.

--- app/ProductionDetail.php
+++ app/ProductionDetail.php
@@ -74,7 +74,6 @@ class ProductionDetail extends Model
                                 ->leftJoin('products', 'productions.product_id', 'products.id')
                                 ->where('status', 3)
                                 ->orWhere('status', 4)
-                                ->orderBy('id', 'desc')
                                 ->get();
     }
 
