150abb7cf95cda05c3aef776399d3cd4795e3938
Darma Wiryatama <wirya_tama99@hotmail.com>
2020-09-23 12:55:16 +0800

    Product return shipping scan null

--- app/ProductionDetail.php
+++ app/ProductionDetail.php
@@ -218,8 +218,11 @@ class ProductionDetail extends Model
     static function rusakUpdate($request)
     {
         ProductionDetail::where('code', $request->barcode)->update([
-            'status'    => $request->status,
-            'nb'        => $request->nb
+            'production_scan'   => Carbon::now(),
+            'admin_scan'        => Carbon::now(),
+            'shipping_scan'     => NULL,
+            'status'            => $request->status,
+            'nb'                => $request->nb
         ]);
     }
 
